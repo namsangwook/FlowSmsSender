afca2747b3f3d7f0ed8cedbd99b7ca3de220c640
namsangwook <flowgrammer@gmail.com>
2016-05-15 22:43:01 +0900

    first commit

---
+++ app/src/main/java/net/flowgrammer/flowsmssender/JobsListActivity.java
@@ -0,0 +1,69 @@
+package net.flowgrammer.flowsmssender;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import net.flowgrammer.flowsmssender.util.Setting;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+/**
+ * Created by neox on 5/15/16.
+ */
+public class JobsListActivity extends AppCompatActivity {
+
+    private static final String QUERY_URL = "http://10.0.2.2:3003/api";
+
+    Setting mSetting;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jobs);
+        mSetting = new Setting(getApplicationContext());
+        loadJobsList();
+
+    }
+
+    private void loadJobsList() {
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        RequestParams params = new RequestParams();
+        params.put("session", mSetting.authKey());
+
+        client.post(QUERY_URL + "/jobs", params, new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(JSONObject response) {
+                Log.d("TEST", "success");
+//                        Log.d("TEST", response.toString());
+                String result = response.optString("result");
+                if (result.equalsIgnoreCase("fail")) {
+                    String message = response.optString("message");
+                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+                    return;
+                }
+                JSONArray list = response.optJSONArray("list");
+                Log.d("TEST", list.toString());
+                if (result.equalsIgnoreCase("success")) {
+                    Toast.makeText(getApplicationContext(), "success", Toast.LENGTH_LONG).show();
+                }
+//                        Log.d("TEST", response.toString());
+//                        super.onSuccess(response);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Throwable e, JSONObject errorResponse) {
+                super.onFailure(statusCode, e, errorResponse);
+                Toast.makeText(getApplicationContext(), errorResponse == null ? "" : errorResponse.toString(), Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/net/flowgrammer/flowsmssender/util/Setting.java
@@ -0,0 +1,34 @@
+package net.flowgrammer.flowsmssender.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by neox on 5/15/16.
+ */
+public class Setting {
+
+    private static final String PREFS = "prefs";
+
+    Context mContext;
+    SharedPreferences mSharedPreferences;
+
+    public Setting(Context context) {
+        mContext = context;
+        mSharedPreferences = mContext.getSharedPreferences(PREFS, mContext.MODE_PRIVATE);
+    }
+
+    public String authKey() {
+//        SharedPreferences mSharedPreferences = mContext.getSharedPreferences(PREFS, mContext.MODE_PRIVATE);
+        String auth = mSharedPreferences.getString("auth_key", "");
+        return auth;
+    }
+
+    public void setAuthKey(String authKey) {
+//        SharedPreferences mSharedPreferences = mContext.getSharedPreferences(PREFS, mContext.MODE_PRIVATE);
+        SharedPreferences.Editor e = mSharedPreferences.edit();
+        e.putString("auth_key", authKey);
+        e.commit();
+    }
+
+}
